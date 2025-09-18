5ee27454d2040ff3cfaf7fd0d935af6af595c4db
Abhishek Kumar <abhishekofficial7050@gmail.com>
2025-09-18 17:26:32 +0530

    change the icon

--- xero_integration_odoo18.0.0/__manifest__.py
+++ xero_integration_odoo18.0.0/__manifest__.py
@@ -19,7 +19,7 @@
         'views/payments_import_export_wizard.xml',
     ],
     'images': [
-    'static/description/image18.png',  # This will be your app icon
+    'static/description/editedImage.png',  # This will be your app icon
     ],
 
     'installable': True,
