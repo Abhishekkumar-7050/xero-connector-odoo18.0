208a8708d84446a571e23a39feee7288112f0d3d
Abhishek Kumar <abhishekofficial7050@gmail.com>
2025-09-18 15:31:10 +0530

    license

--- xero_integration_odoo18.0.0/__manifest__.py
+++ xero_integration_odoo18.0.0/__manifest__.py
@@ -5,7 +5,8 @@
     'summary': "Integrate Odoo with Xero Accounting",
     'author': " Abhishek Kumar ",
     'web_icon': 'xero_integration_odoo18.0.0,static/src/img/icon.png',
-    'depends': ['base', 'account','mail'], # 'account' zaroori hai
+    'license': 'OPL-1',
+    'depends': ['base', 'account','mail'], 
 
     'data': [
         'security/ir.model.access.csv',
@@ -20,7 +21,7 @@
     'images': [
     'static/description/icon.png',  # This will be your app icon
     ],
-    
+
     'installable': True,
     'application': True,
 }
